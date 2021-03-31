bff0c11f1ee50c7b476132e2d72ca36298b5a63b
xXxSpicyBoiiixXx <mali54@hawk.iit.edu>
2021-03-31 15:57:50 -0500

    add

--- Learning-Rust/enums/src/main.rs
+++ Learning-Rust/enums/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+// Enums data structures can only be one of its variants. 
+// Both IPv4 and IPv6 are basically the same type, but they 
+// treated the same. 
+
+/*
+enum IPAddrKind {
+	V4,
+	V6
 }
+
+struct IpAddr {
+	kind: IpAddrKind,
+	address: String,
+}
+
+let home = IpAddr {
+	kind: IpAddrKind::V4,
+	address: String::from("127.0.0.1"),
+};
+
+let loopback = IpAddr {
+	kind: IpAddrKind::V6,
+	address: String::from("::1"),
+};
+
+*/
+
+// We can make the above in a more concise way as follows 
+/*
+enum IpAddr {
+ 	
+	// We know that version 4 IP addresses will always have four numeric
+	// components that will have values between 0 and 255. So we need
+	// the V4 to be four separate u8 values. 
+     	// V4(String), 
+	V4(u8, u8, u8, u8),
+	V6(String),
+}
+*/
+
+struct Ipv4Addr {
+	// --snip--
+}
+
+struct Ipv6Addr {
+	// --snip-- 
+}
+// These are similar to the enum poriton
+struct QuitMEssage; // unit struct 
+
+struct MoveMessage { 
+	x: i32,
+	y: i32,
+}
+
+struct WriteMessage(String); // tuple struct 
+
+struct ChangeColorMessage(i32, i32, i32);
+
+enum IpAddr { 
+	V4(Ipv4Addr),
+	V6(Ipv6Addr),
+}
+
+enum Message { 
+	Quit, 
+	Move { x: i32, y: i32 }, 
+	Write(String) 
+	ChangeColor(i32, i32, i32),	
+}
+fn main() {
+
+let four = IpAddrKind::V4;
+let six = IpAddrKind::V6; 
+
+route(IPAddrKind::V4)
+route(IpAddrKind::V6)
+    
+}
+
+// The bottom is the previous version for IPv4
+// let home = IpAddr::V4(String::from("127.0.0.1"));
+
+let home IpAddr::V4(127,0,0,1); 
+
+let loopback = IpAddr::V6(String::from("::1"));
+
+fn route(ip_kind: IpAddrKind) {} 
