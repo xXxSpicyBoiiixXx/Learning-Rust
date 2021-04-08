e49d9f66907d0ef8086d5f6270249098f9e3e054
xXxSpicyBoiiixXx <mali54@hawk.iit.edu>
2021-04-08 16:03:46 -0500

    updating library

--- Learning-Rust/my-project/restaurant/src/lib.rs
+++ Learning-Rust/my-project/restaurant/src/lib.rs
@@ -1,7 +1,9 @@
 mod front_of_house {
 	pub mod hosting { 
-		fn add_to_waitlist() {}
-	
+		// fn add_to_waitlist() {}
+		
+		pub fn add_to_waitlist() {} 
+
 		fn seat_at_table() {}
 	}
 
@@ -14,14 +16,14 @@ mod front_of_house {
 	}
 }
 
-pub fn eat_at_restaurant() {
+/*pub fn eat_at_restaurant() {
 	
 	// Absolute Path 
 	crate::front_of_house::hosting::add_to_waitlist();
 	
 	// Relative Path 
 	front_of_house::hosting::add_to_waitlist();
-}
+}*/
 
 fn server_order() {}
 
@@ -33,4 +35,49 @@ mod back_of_house {
 
 	fn cook_order() {}
 
+	pub struct Breakfast {
+		pub toast: String, 
+		seasonal_fruit: String,
+	}
+
+	impl Breakfast {
+		pub fn summer(toast: &str) -> Breakfast {
+			Breakfast {
+				toast: String::from(toast),
+				seasonal_fruit: String::from("peaches"),
+			}
+		}
+	}
+	
+	pub enum Appetizer {
+		Soup, 
+		Salad,
+	}
 }
+
+use crate::front_of_house::hosting;
+
+// You can also bring the crate into schope with the following command 
+// use self::front_of_house::hosting; 
+
+pub fn eat_at_restaurant() {
+	
+	let mut meal = back_of_house::Breakfast::summer("Rye");	
+	meal.toast = String::from("Wheat");	
+	println!("I'd like {} toast please", meal.toast);
+
+	/* 
+	 * The next line won't compile if we uncomment it, we're not allowed
+	 * to see or modify the seasonal fruit that comes with the meal 
+	 * meal.seasonal_fruit = String::from("blueberries");
+	 */ 
+
+	let order1 = back_of_house::Appetizer::Soup;
+	let order2 = back_of_house::Appetizer::Salad;
+
+	hosting::add_to_waitlist();
+	hosting::add_to_waitlist();
+	hosting::add_to_waitlist();
+}
+
+
